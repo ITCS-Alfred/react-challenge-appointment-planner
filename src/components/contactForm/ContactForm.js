7855e5bfe2816aa79988037660a8fb53a5b3c0e8
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-09-07 14:41:29 +0100

    created the Contact Form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,31 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <div className="inputForm" >
+    <form onSubmit={handleSubmit}>
+      <input  
+      value={name}
+      className="name"
+      type="text"
+      onChange={(e) => setName(e.target.value)}
+      />
+      <input  
+      value={phone}
+      onChange={(e) => setPhone(e.target.value)}
+      className="phone"
+      type="tel"
+      pattern="^[2-9]\d{2}-\d{3}-\d{4}$"
+      />
+       <input  
+       value={email}
+      className="email"
+      type="email"
+      onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+      type="submit"
+      />
+      </form>
+    </div>
   );
 };
